45caef115f5cf13b2d7da7c1b338aa18b0ade023
Ben Christel <bchristel@pivotal.io>
2017-06-18 22:57:15 -0700

    Add a primitive ranking system for webpages
    - plain HTML ranks high; CSS and JavaScript deduct from the score
    - only pages in English are crawled
    - .edu domains get a boost in the rankings

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var supercrawler = require("@benchristel/supercrawler")
 var cheerio = require('cheerio')
+var languageDetector = new (require('languagedetect'))
+var url = require('url')
+var excludedHostnames = require('./excludedHostnames')
 
 var crawler = new supercrawler.Crawler({
   // By default, Supercrawler uses a simple
@@ -31,33 +34,132 @@ var crawler = new supercrawler.Crawler({
   userAgent: "Mozilla/5.0 (compatible; supercrawler/1.0; +https://github.com/brendonboshell/supercrawler)"
 })
 
-// Get "Sitemaps:" directives from robots.txt
-// crawler.addHandler(supercrawler.handlers.robotsParser())
-
-// Crawl sitemap files and extract their URLs.
-// crawler.addHandler(supercrawler.handlers.sitemapsParser())
-
 // Pick up <a href> links from HTML documents
-crawler.addHandler("text/html", supercrawler.handlers.htmlLinkParser({
-  // Optional: restrict queued links to certain hostnames:
-  // hostnames: ["example.com"]
-  excludedHostnames: ['twitter.com', 'facebook.com', 'google.com', 'github.com', 'amazon.com']
-}))
+crawler.addHandler("text/html",
+  JudgementalLinkParser({
+    judges:
+      [ isThisEvenEnglish
+      , isThisEvenHtml
+      , isThisAnEduDomain
+      , isThereTooMuchCss
+      , isThereTooMuchJavaScript
+      ],
+    minRating: 0,
+    parser: supercrawler.handlers.htmlLinkParser({
+      excludedHostnames: excludedHostnames
+    })
+  }))
 
 // Custom content handler for HTML pages.
-crawler.addHandler("text/html", function (context) {
-  var $ = cheerio.load(context.body)
+crawler.addHandler("text/html", function (page) {
+  page.$ = page.$ || cheerio.load(page.body)
+  var $ = page.$
   console.log("Processed", $('title').text())
 })
 
-// .insertIfNotExists(new supercrawler.Url("http://uea.ac.uk/"))
-
 crawler.getUrlList()
-  .insertIfNotExists(new supercrawler.Url("http://zompist.com"))
+  .insertIfNotExists(new supercrawler.Url("http://www2.cmp.uea.ac.uk/~jrk/conlang.html"))
   .then(function() {
-    return crawler.getUrlList().insertIfNotExists(new supercrawler.Url("http://www2.cmp.uea.ac.uk/~jrk/conlang.html"))
+    return crawler.getUrlList().insertIfNotExists(new supercrawler.Url("http://zompist.com"))
   })
   .then(function () {
     console.log('started')
     return crawler.start();
   });
+
+function isThisEvenEnglish(page) {
+  page.$ = page.$ || cheerio.load(page.body)
+  var text = page.$('body').text()
+
+  var mostLikelyLanguage = languageDetector.detect(text, 1)[0]
+  if (mostLikelyLanguage && mostLikelyLanguage[0] === 'english') {
+    return 0
+  } else {
+    return -Infinity
+  }
+}
+
+function isThisEvenHtml(page) {
+  var score = 0
+  if (/\.html?\??#?$/.test(page.url)) {
+    score += 32000
+  } else if (/\....?\??#?$/.test(url.parse(page.url)).pathname) {
+    score -= 64000
+  }
+  page.$ = page.$ || cheerio.load(page.body)
+  var paragraphs = page.$('p').length
+  if (paragraphs.length < 100) {
+    score += paragraphs * 256
+  } else {
+    score += 25600
+  }
+
+  console.log('html score:', score)
+
+  return score
+}
+
+function isThisAnEduDomain(page) {
+  if (/\.edu$/.test(url.parse(page.url).hostname)) {
+    console.log('edu score: ', 64000)
+    return 64000
+  } else {
+    return 0
+  }
+}
+
+function isThereTooMuchJavaScript(page) {
+  page.$ = page.$ || cheerio.load(page.body)
+  var score = 0
+
+  var scripts = page.$('script[src]')
+  score += scripts.length * -6000
+
+  scripts = page.$('script:not([src])')
+  scripts.each(function(i, script) {
+    score -= page.$(script).text().length
+  })
+
+  console.log('javascript score: ', score)
+
+  return score
+}
+
+function isThereTooMuchCss(page) {
+  page.$ = page.$ || cheerio.load(page.body)
+  var styles = page.$('link[rel=stylesheet]')
+  var score = styles.length * -17000
+  console.log('css score: ', score)
+  return score
+}
+
+function JudgementalLinkParser(opts) {
+  var parser    = opts.parser
+  var minRating = opts.minRating
+  var judges    = opts.judges
+
+  return function(pageData) {
+    if (isWorthy(pageData)) {
+      return parser(pageData)
+    }
+    return []
+  }
+
+  function isWorthy(pageData) {
+    var rating = judges
+      .map(judge => judge(pageData))
+      .reduce(add, 0)
+
+    if (rating >= minRating) {
+      console.log('=== good (' + rating + '): ', pageData.url)
+    } else {
+      console.log('<<< bad  (' + rating + '): ', pageData.url)
+    }
+
+    return rating >= minRating
+  }
+
+  function add(a, b) {
+    return a + b
+  }
+}
